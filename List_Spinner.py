58d4262cd1bdd674668664c3504ec032a1d07ba3
GiovaldiRChaniago <46389340+giovaldi@users.noreply.github.com>
2020-06-04 13:10:25 +0700

    Update List_Spinner.py

--- List_Spinner.py
+++ List_Spinner.py
@@ -3,7 +3,7 @@ def counterClockwise(list_awal): #fungsi def yang menerima satu argumen yaitu li
     puterr = [] #list kosong untuk menerima hasil iterasi dari list_awal
     for i in range(len(list_awal[0]), 0, -1):#for loop yang mengiterasi di range len(list_awal[0]) = 3, 0 sebagai exclussive, dan -1 sebagai stepnya
         rumusan = list(map(lambda x: x[i-1], list_awal)) #lambda yang menerima argumen x[i-1] untuk mendapatkan puterr contoh : x[3-1], x[2-1] di variabel list_awal
-        puterr.append(rumusan)
+        puterr.append(rumusan) #menambahkan nilai rumusan ke puterr dengan.append()
     return puterr#mengembalikan nilai puterr
 
 print(counterClockwise([[1, 2, 3],
