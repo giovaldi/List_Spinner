33fe6e2389b4e99851fa2939cb51cca94c499ff3
GiovaldiRChaniago <46389340+giovaldi@users.noreply.github.com>
2020-06-04 12:44:07 +0700

    Add files via upload

---
+++ List_Spinner.py
@@ -0,0 +1,11 @@
+#logicnya mirip sama spin matrix yang menerima nilai inisiasi z ='' kemudian menggunakan loop untuk mencari iterasinya kemudian print z
+def counterClockwise(list_awal): #fungsi def yang menerima satu argumen yaitu list awal
+    puterr = [] #list kosong untuk menerima hasil iterasi dari list_awal
+    for i in range(len(list_awal[0]), 0, -1):#for loop yang mengiterasi di range len(list_awal[0]) = 3, 0 sebagai exclussive, dan -1 sebagai stepnya
+        rumusan = list(map(lambda x: x[i-1], list_awal)) #lambda yang menerima argumen x[i-1] untuk mendapatkan puterr contoh : x[3-1], x[2-1] di variabel list_awal
+        puterr.append(rumusan)
+    return puterr#mengembalikan nilai puterr
+
+print(counterClockwise([[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]]))
